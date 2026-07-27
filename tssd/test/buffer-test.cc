8374413643d23d7549110f4a4d6309fc011d927e
BrookZhou <tssdorg@hotmail.com>
2026-07-27 11:10:32 +0000

    add Buffer dump UTs

--- tssd/test/buffer-test.cc
+++ tssd/test/buffer-test.cc
@@ -23,10 +23,7 @@ std::vector<Bytes> getData(Buffer &buf)
     return result;
 }
 
-bool appendTest(size_t mtu, int n,  std::vector<Bytes> expect)
-{
-    Buffer buf;
-    buf.mtu_ = mtu;
+void append(Buffer &buf, int n) {
     std::vector<std::byte> vin(n);
     for (int i=0; i<n; i++)
         vin[i] = std::byte(100 + i);
@@ -38,7 +35,13 @@ bool appendTest(size_t mtu, int n,  std::vector<Bytes> expect)
     buf.append(&vin[rd], n-rd);
 
     buf.finish();
+}
 
+bool appendTest(size_t mtu, int n,  std::vector<Bytes> expect)
+{
+    Buffer buf;
+    buf.mtu_ = mtu;
+    append(buf, n);
     auto result = getData(buf);
 
     if (result.size() != expect.size()) {
@@ -51,13 +54,10 @@ bool appendTest(size_t mtu, int n,  std::vector<Bytes> expect)
             cout << "appendTest i:" << i << endl;
             return false;
         }
-
-
     return true;
 }
 
 TEST(Buffer, append) {
-
     EXPECT_TRUE(appendTest(1, 1, vector<Bytes> {
                         Bytes {byte(100)},
                     }
@@ -135,5 +135,38 @@ TEST(Buffer, append) {
                         Bytes{ byte(110), byte(111), byte(112), byte(113)}
                     }
     ));
+}
 
+bool dumpTest(Buffer &buf, int n, Bytes expect)
+{
+    byte bs[100];
+    if (buf.dump(n, bs)) return false;
+    return Basic::BytesEqual(expect, std::span<byte>(bs, n));
+}
+
+TEST(Buffer, dump) {
+    Buffer buf(5);
+    append(buf, 14);
+
+    EXPECT_TRUE(dumpTest(buf, 1, Bytes{byte(100)}));
+    EXPECT_TRUE(dumpTest(buf, 2, Bytes{byte(101), byte(102)}));
+    EXPECT_TRUE(dumpTest(buf, 2, Bytes{byte(103), byte(104)}));
+    EXPECT_TRUE(dumpTest(buf, 1, Bytes{byte(105)}));
+    EXPECT_TRUE(dumpTest(buf, 3, Bytes{byte(106), byte(107), byte(108)}));
+    EXPECT_TRUE(dumpTest(buf, 4, Bytes{byte(109), byte(110), byte(111), byte(112)}));
+    EXPECT_FALSE(dumpTest(buf, 2, Bytes{byte(101), byte(102)}));
+}
+
+TEST(Buffer, dump2) {
+    Buffer buf(5);
+    append(buf, 16);
+    EXPECT_TRUE(dumpTest(buf, 1, Bytes{byte(100)}));
+    EXPECT_TRUE(dumpTest(buf, 9, Bytes{byte(101), byte(102), byte(103), byte(104), byte(105), byte(106), byte(107), byte(108), byte(109)}));
+}
+
+TEST(Buffer, dump3) {
+    Buffer buf(5);
+    append(buf, 16);
+    EXPECT_TRUE(dumpTest(buf, 1, Bytes{byte(100)}));
+    EXPECT_TRUE(dumpTest(buf, 10, Bytes{byte(101), byte(102), byte(103), byte(104), byte(105), byte(106), byte(107), byte(108), byte(109), byte(110)}));
 }
